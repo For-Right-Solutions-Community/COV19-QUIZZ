e3e66e1b964fe88824614a4fde3db4a5943bf0f8
Samih Chouchen <chouchensamih@gmail.com>
2020-03-29 16:52:10 +0100

    finition de l'interface quizz

--- src/components/Quizz.vue
+++ src/components/Quizz.vue
@@ -1,115 +1,128 @@
 <template >
-<section class="container">
+  <section class="container">
+    <div class="columns grid-xs">
+      <div class="column col-mx-auto col-4 col-xs-12 col-xl-4 col-l-5 col-md-8">
+        <div class="questionBox" id="quizz">
+          <div class="panel">
+            <div class="panel-header">
+              <div class="panel-title column">
+                <h1 class="title is-6 columns col-mx-auto">Fiche de Symptômes</h1>
+              </div>
+            </div>
+            <div class="panel-nav">
+              <!-- navigation components: tabs, breadcrumbs or pagination -->
+            </div>
+            <div class="panel-body">
+              <!--qusetionContainer-->
+              <div
+                class="questionContainer"
+                v-if="questionIndex<quiz.questions.length"
+                v-bind:key="questionIndex"
+              >
+                <!--progress-->
+                <div class="progressContainer">
+                  <progress
+                    class="progress is-info is-small"
+                    :value="(questionIndex/quiz.questions.length)*100"
+                    max="100"
+                  >{{(quiz.questions.length)*100}}%</progress>
+                </div>
+                <!--/progress-->
 
-  <div class="questionBox" id="quizz">
-    <!-- transition -->
-    <transition
-      :duration="{ enter: 100, leave: 100 }"
-      enter-active-class="animated zoomIn"
-      leave-active-class="animated zoomOut"
-      mode="out-in"
-    >
-      <!--qusetionContainer-->
-      <div
-        class="questionContainer"
-        v-if="questionIndex<quiz.questions.length"
-        v-bind:key="questionIndex"
-      >
-        <header>
-          <h1 class="title is-6">Test COVID19</h1>
-          <!--progress-->
-          <div class="progressContainer">
-            <progress
-              class="progress is-info is-small"
-              :value="(questionIndex/quiz.questions.length)*100"
-              max="100"
-            >{{(quiz.questions.length)*100}}%</progress>
-          </div>
-          <!--/progress-->
-        </header>
+                <!-- questionTitle -->
+                <h2
+                  class="titleContainer title label label-rounded label-primary"
+                >{{ quiz.questions[questionIndex].text }}</h2>
 
-        <!-- questionTitle -->
-        <h2 class="titleContainer title">{{ quiz.questions[questionIndex].text }}</h2>
+                <!-- quizOptions -->
+                <div class="optionContainer" align="right">
+                  <template v-if="quiz.questions[questionIndex].QUIZZ_TYPE=='ONECHOICE'">
+                    <div
+                      class="option"
+                      v-for="(response, index) in quiz.questions[questionIndex].responses"
+                      @click="selectOption(index)"
+                      :class="{ 'is-selected': userResponses[questionIndex] == index}"
+                      :key="index"
+                    >{{ response.text }}</div>
+                  </template>
 
-        <!-- quizOptions -->
-        <div class="optionContainer" align="right">
-          <template v-if="quiz.questions[questionIndex].QUIZZ_TYPE=='ONECHOICE'">
-            <div
-              class="option"
-              v-for="(response, index) in quiz.questions[questionIndex].responses"
-              @click="selectOption(index)"
-              :class="{ 'is-selected': userResponses[questionIndex] == index}"
-              :key="index"
-            >{{ response.text }}</div>
-          </template>
+                  <div v-if="quiz.questions[questionIndex].QUIZZ_TYPE=='FORMULAIRE'">
+                    <div
+                      class="option"
+                      v-for="(formulaire, index) in quiz.questions[questionIndex].formulaires"
+                      :key="index"
+                    >
+                      <input type="text" :placeholder="formulaire.label" :value=" quiz.user " />
+                    </div>
+                  </div>
 
-          <div v-if="quiz.questions[questionIndex].QUIZZ_TYPE=='FORMULAIRE'">
-            <div class="option" v-for="(formulaire, index) in quiz.questions[questionIndex].formulaires" :key="index">
-              <input type="text" :placeholder="formulaire.label" :value=" quiz.user " />
+                  <div v-if="quiz.questions[questionIndex].QUIZZ_TYPE=='MULTIPLECHOICES'">
+                    <div
+                      v-for="(response, index) in quiz.questions[questionIndex].responses"
+                      :key="index"
+                    >
+                      {{ response.text }}
+                      <input
+                        type="checkbox"
+                        @click="selectMulipleOption(index)"
+                        v-model="quiz.questions[questionIndex].responses[index].selected"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!--/questionContainer-->
+
+              <!--quizCompletedResult-->
+              <div
+                v-if="questionIndex >= quiz.questions.length"
+                v-bind:key="questionIndex"
+                class="quizCompleted has-text-centered"
+              >
+                <!-- quizCompletedIcon: Achievement Icon -->
+                <span class="icon">
+                  <i class="fa" :class="score()>3?'fa-check-circle-o is-active':'fa-times-circle'"></i>
+                </span>
+
+                <!--resultTitleBlock-->
+                <h2 class="title">Best Score</h2>
+                <!--<p class="subtitle">
+					Total score: {{ score() }} / {{ quiz.questions.length }}
+                </p>-->
+                <br />
+                <a class="button" @click="restart()">
+                  restart
+                  <i class="fa fa-refresh"></i>
+                </a>
+                <!--/resultTitleBlock-->
+              </div>
+            </div>
+            <div class="container">
+              <header class="navbar">
+                <section class="navbar-section">
+                  <button v-on:click="prev();" :disabled="questionIndex < 1" class="btn btn-primary">
+                    Précedent
+                    <i class="icon icon-arrow-left"></i>
+                  </button>
+                </section>
+                <section class="navbar-center">
+                  <!-- centered logo or brand -->
+                </section>
+                <section class="navbar-section">
+                  <button :class="(userResponses[questionIndex]==null)?'':'is-active'"
+                    v-on:click="next();"
+                    :disabled="questionIndex>=quiz.questions.length || userResponses[questionIndex]==null " class="btn btn-primary">
+                    Suivant
+                    <i class="icon icon-arrow-right"></i>
+                  </button>
+                </section>
+              </header>
+              <!--/quizFooter-->
             </div>
           </div>
-
-          <div v-if="quiz.questions[questionIndex].QUIZZ_TYPE=='MULTIPLECHOICES'">
-          <div v-for="(response, index) in quiz.questions[questionIndex].responses" :key="index">
-            {{ response.text }}
-            <input
-              type="checkbox"
-              @click="selectMulipleOption(index)"
-              v-model="quiz.questions[questionIndex].responses[index].selected"
-            />
-          </div>
-          </div>
-
         </div>
-
-        <!--quizFooter: navigation and progress-->
-        <footer class="questionFooter">
-          <!--pagination-->
-          <nav class="pagination" role="navigation" aria-label="pagination">
-            <!-- back button -->
-            <a class="button" v-on:click="prev();" :disabled="questionIndex < 1">Retour</a>
-
-            <!-- next button -->
-            <a
-              class="button"
-              :class="(userResponses[questionIndex]==null)?'':'is-active'"
-              v-on:click="next();"
-              :disabled="questionIndex>=quiz.questions.length"
-            >{{ (userResponses[questionIndex]==null)?'Enregister et Contiuner':'Suivant' }}</a>
-          </nav>
-          <!--/pagination-->
-        </footer>
-        <!--/quizFooter-->
       </div>
-      <!--/questionContainer-->
-
-      <!--quizCompletedResult-->
-      <div
-        v-if="questionIndex >= quiz.questions.length"
-        v-bind:key="questionIndex"
-        class="quizCompleted has-text-centered"
-      >
-        <!-- quizCompletedIcon: Achievement Icon -->
-        <span class="icon">
-          <i class="fa" :class="score()>3?'fa-check-circle-o is-active':'fa-times-circle'"></i>
-        </span>
-
-        <!--resultTitleBlock-->
-        <h2 class="title"> Best Score </h2>
-        <!--<p class="subtitle">
-					Total score: {{ score() }} / {{ quiz.questions.length }}
-        </p>-->
-        <br />
-        <a class="button" @click="restart()">
-          restart
-          <i class="fa fa-refresh"></i>
-        </a>
-        <!--/resultTitleBlock-->
-      </div>
-      <!--/quizCompetedResult-->
-    </transition>
-
-  </div>
+    </div>
   </section>
 </template>
 
@@ -484,132 +497,6 @@ var quizz = {
 </script>
 
 <style scoped>
-body {
-  font-size: 14px;
-  height: 100vh;
-  background: rgb(172, 22, 80);
-  /* mocking native UI */
-  cursor: default !important;
-  /* remove text selection cursor */
-  -webkit-user-select: none;
-     -moz-user-select: none;
-      -ms-user-select: none;
-          user-select: none;
-  /* remove text selection */
-  user-drag: none;
-  /* disbale element dragging */
-  display: -webkit-box;
-  display: flex;
-  -webkit-box-align: center;
-          align-items: center;
-  -webkit-box-pack: center;
-          justify-content: center;
-}
-
-.button {
-  -webkit-transition: 0.3s;
-  transition: 0.3s;
-}
-
-.title,
-.subtitle {
-  font-family: Montserrat, sans-serif;
-  font-weight: normal;
-}
-
-.animated {
-  -webkit-transition-duration: 0.15s;
-          transition-duration: 0.15s;
-}
-
-.container {
-  margin: 0 0.5rem;
-}
-
-.questionBox {
-  max-width: 30rem;
-  width: 30rem;
-  min-height: 30rem;
-  background: #FAFAFA;
-  position: relative;
-  display: -webkit-box;
-  display: flex;
-  border-radius: 0.5rem;
-  overflow: hidden;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-}
-.questionBox header {
-  background: rgba(0, 0, 0, 0.025);
-  padding: 1.5rem;
-  text-align: center;
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
-}
-.questionBox header h1 {
-  font-weight: bold;
-  margin-bottom: 1rem !important;
-}
-.questionBox header .progressContainer {
-  width: 60%;
-  margin: 0 auto;
-}
-.questionBox header .progressContainer > progress {
-  margin: 0;
-  border-radius: 5rem;
-  overflow: hidden;
-  border: none;
-  color: #3D5AFE;
-}
-.questionBox header .progressContainer > progress::-moz-progress-bar {
-  background: #3D5AFE;
-}
-.questionBox header .progressContainer > progress::-webkit-progress-value {
-  background: #3D5AFE;
-}
-.questionBox header .progressContainer > p {
-  margin: 0;
-  margin-top: 0.5rem;
-}
-.questionBox .titleContainer {
-  text-align: center;
-  margin: 0 auto;
-  padding: 1.5rem;
-}
-.questionBox .quizForm {
-  display: block;
-  white-space: normal;
-  height: 100%;
-  width: 100%;
-}
-.questionBox .quizForm .quizFormContainer {
-  height: 100%;
-  margin: 15px 18px;
-}
-.questionBox .quizForm .quizFormContainer .field-label {
-  text-align: left;
-  margin-bottom: 0.5rem;
-}
-.questionBox .quizCompleted {
-  width: 100%;
-  padding: 1rem;
-  text-align: center;
-}
-.questionBox .quizCompleted > .icon {
-  color: #FF5252;
-  font-size: 5rem;
-}
-.questionBox .quizCompleted > .icon .is-active {
-  color: #00E676;
-}
-.questionBox .questionContainer {
-  white-space: normal;
-  height: 100%;
-  width: 100%;
-}
-.questionBox .questionContainer .optionContainer {
-  margin-top: 12px;
-  -webkit-box-flex: 1;
-          flex-grow: 1;
-}
 .questionBox .questionContainer .optionContainer .option {
   border-radius: 290486px;
   padding: 9px 18px;
@@ -631,66 +518,6 @@ body {
 }
 .questionBox .questionContainer .optionContainer .option:active {
   -webkit-transform: scaleX(0.9);
-          transform: scaleX(0.9);
-}
-.questionBox .questionContainer .questionFooter {
-  background: rgba(0, 0, 0, 0.025);
-  border-top: 1px solid rgba(0, 0, 0, 0.1);
-  width: 100%;
-  align-self: flex-end;
-}
-.questionBox .questionContainer .questionFooter .pagination {
-  margin: 15px 25px;
-}
-
-.pagination {
-  display: -webkit-box;
-  display: flex;
-  -webkit-box-pack: justify;
-          justify-content: space-between;
-}
-
-.button {
-  padding: 0.5rem 1rem;
-  border: 1px solid rgba(0, 0, 0, 0.25);
-  border-radius: 5rem;
-  margin: 0 0.25rem;
-  -webkit-transition: 0.3s;
-  transition: 0.3s;
-}
-.button:hover {
-  cursor: pointer;
-  background: #ECEFF1;
-  border-color: rgba(0, 0, 0, 0.25);
-}
-.button.is-active {
-  background: #3D5AFE;
-  color: white;
-  border-color: transparent;
-}
-.button.is-active:hover {
-  background: #0a2ffe;
-}
-
-@media screen and (min-width: 769px) {
-  .questionBox {
-    -webkit-box-align: center;
-            align-items: center;
-    -webkit-box-pack: center;
-            justify-content: center;
-  }
-  .questionBox .questionContainer {
-    display: -webkit-box;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-            flex-direction: column;
-  }
-}
-@media screen and (max-width: 768px) {
-  .sidebar {
-    height: auto !important;
-    border-radius: 6px 6px 0px 0px;
-  }
+  transform: scaleX(0.9);
 }
 </style>
